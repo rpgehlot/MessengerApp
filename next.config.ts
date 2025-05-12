692732045564bba4ff64aa222649f70af9437bdc
Rituparn Gehlot <rpgehlot1991@gmail.com>
2025-05-12 16:16:33 +0530

    Enabled browser source maps for production/preview urls

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode : false,
+  productionBrowserSourceMaps : true,
   typescript : {
     ignoreBuildErrors : true
   },
